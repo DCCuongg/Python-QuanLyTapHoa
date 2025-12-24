6e09caba5b9d77421aeefbb395415c79300d905c
DCCuong <cuongfc1231232005@gmail.com>
2025-12-25 00:26:03 +0700

    Cac api hang_hoa

--- QuanLyTapHoa/QuanLyHangHoa/urls.py
+++ QuanLyTapHoa/QuanLyHangHoa/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import hanghoa_get_all
+from .views import *
 
 urlpatterns = [
     path('hanghoa/', hanghoa_get_all),
+    path('hanghoa/<int:ma_hang>/', hanghoa_get_by_id),
+    path('hanghoa/create/', hanghoa_create),
+    path('hanghoa/<int:ma_hang>/update/', hanghoa_update),
+    path('hanghoa/<int:ma_hang>/delete/', hanghoa_delete),
+    path('hanghoa/<int:ma_hang>/adjust-stock/', hanghoa_adjust_stock),
 ]
